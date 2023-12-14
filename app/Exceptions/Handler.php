020f319a10d496bf4a56bd31bb5823cb9042975d
azmammad.q <azmammad.q@kripton.az>
2023-12-14 19:13:23 +0400

    exception handling added

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,14 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Response;
+use Exception;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
 
 class Handler extends ExceptionHandler
 {
@@ -26,5 +34,31 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+
+        if ($exception instanceof ValidationException) {
+            Log::error("Validation Exception: " . $exception->getMessage());
+            return error(Response::HTTP_UNPROCESSABLE_ENTITY, "Daxil edilən məlumatlar tam deyil.", $exception->errors());
+        }
+
+        if ($exception instanceof NotFoundHttpException || $exception instanceof ModelNotFoundException) {
+            Log::error("NotFound Exception: " . $exception->getMessage());
+            return error(Response::HTTP_NOT_FOUND, "Məlumat tapılmadı.", $exception->getMessage());
+        }
+
+        if ($exception instanceof AuthenticationException) {
+            Log::error("Authentication Exception: " . $exception->getMessage());
+            return error(Response::HTTP_UNAUTHORIZED, "Not authenticated.", $exception->getMessage());
+        }
+
+        if ($exception instanceof Exception) {
+            Log::error("Internal Server Exception: " . $exception->getMessage());
+            return error(Response::HTTP_INTERNAL_SERVER_ERROR, "Server xətası.", $exception->getMessage());
+        }
+
+        return parent::render($request, $exception);
     }
 }
